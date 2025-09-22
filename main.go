37955571ba29500153b77b35b64115fce674298b
Raezil <kamil.mazurek319@gmail.com>
2025-09-22 18:49:06 +0200

    fix main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type Options struct {
 func readOptions() *Options {
 	opts := &Options{}
 	opts.path = flag.String("path", "./customers.csv", "Path to the file with customer data")
-	opts.outFile = flag.String("out", "", "Optional: output file path. If empty program will output results to the terminal")
+	opts.outFile = flag.String("out", "./out.csv", "Optional: output file path. If empty program will output results to the terminal")
 	flag.Parse()
 	return opts
 }
